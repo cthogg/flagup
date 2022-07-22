b1d13ad47577c06b54eec6d6442f934c0cffcd56
Christopher Hogg <christopher@luminovo.ai>
2022-07-22 17:26:12 +0100

    feat: get the flag to work

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use console::Emoji;
-use std::collections::HashMap;
+use std::{collections::HashMap, env::Args};
 fn main() {
+    let args = Config::new(std::env::args());
+    let test = args.country_text.clone();
     let mut countries_and_emojis: HashMap<&str, Emoji> = HashMap::new();
     countries_and_emojis.insert("Germany", Emoji::new("🇩🇪", "German flag"));
     countries_and_emojis.insert("France", Emoji::new("🇫🇷", "French flag"));
 
-    let me = match countries_and_emojis.get("Gdermany") {
+    let me = match countries_and_emojis.get(&test as &str) {
         Some(&emoji) => emoji,
         None => Emoji::new("🤷‍♂️", "No emoji"),
     };
 
     println!("{}", me);
+}
 
-    println!("Hello, world!");
+struct Config {
+    pub country_text: String,
+}
+
+impl Config {
+    pub fn new(args: Args) -> Self {
+        let ar: Vec<String> = args.collect();
+        Config {
+            country_text: ar[1].clone(),
+        }
+    }
 }
