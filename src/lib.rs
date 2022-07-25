c910b6fac70c291042ddb4b619c04cd5421930c6
Christopher Hogg <christopher@luminovo.ai>
2022-07-25 08:21:18 +0100

    refactor: make it tidier

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
+use std::{collections::HashMap, env::Args};
 
+pub struct Config {
+    pub country_text: String,
+}
+
+impl Config {
+    pub fn new(args: Args) -> Self {
+        let ar: Vec<String> = args.collect();
+        Config {
+            country_text: ar[1].clone(),
+        }
+    }
+}
+
+pub fn generate_result(args: Config) {
+    let test = args.country_text.clone();
+    let tuples = [("Germany", "🇩🇪"), ("France", "🇫🇷")];
+    let m: HashMap<&str, &str> = tuples.into_iter().collect();
+
+    let flag = match m.get(&test as &str) {
+        Some(&str) => &str,
+        None => "🤷‍♂️",
+    };
+
+    println!("{}", flag);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-use std::{collections::HashMap, env::Args};
+use flag_creator::{generate_result, Config};
 fn main() {
     let args = Config::new(std::env::args());
-    let test = args.country_text.clone();
-    let tuples = [("Germany", "🇩🇪"), ("France", "🇫🇷")];
-    let m: HashMap<&str, &str> = tuples.into_iter().collect();
-
-    let flag = match m.get(&test as &str) {
-        Some(&str) => &str,
-        None => "🤷‍♂️",
-    };
-
-    println!("{}", flag);
-}
-
-struct Config {
-    pub country_text: String,
-}
-
-impl Config {
-    pub fn new(args: Args) -> Self {
-        let ar: Vec<String> = args.collect();
-        Config {
-            country_text: ar[1].clone(),
-        }
-    }
+    generate_result(args);
 }
