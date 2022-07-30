8c6361d64616bf3cef16a26aa3296381d9fbfe9f
Christopher Hogg <christopher@luminovo.ai>
2022-07-30 08:12:49 +0100

    feat: build all

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use flagup::{generate_result, Config};
+use flagup::{generate_result, read_json_file, Config};
 fn main() {
     let args = Config::new(std::env::args().collect());
-    println!("{}", generate_result(args));
+    println!("{}", generate_result(args, read_json_file()));
 }
